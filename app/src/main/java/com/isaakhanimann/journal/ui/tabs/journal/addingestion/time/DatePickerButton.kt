f7540127849fd9874a5ec9f77b32792488af6015
Isaak <31287934+isaakhanimann@users.noreply.github.com>
2024-12-10 08:03:18 +0100

    Start using new compose date picker (wip)

--- app/src/main/java/com/isaakhanimann/journal/ui/tabs/journal/addingestion/time/DatePickerButton.kt
+++ app/src/main/java/com/isaakhanimann/journal/ui/tabs/journal/addingestion/time/DatePickerButton.kt
@@ -18,22 +18,26 @@
 
 package com.isaakhanimann.journal.ui.tabs.journal.addingestion.time
 
-import android.app.DatePickerDialog
-import android.widget.DatePicker
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Event
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.semantics.clearAndSetSemantics
-import com.isaakhanimann.journal.R
+import java.time.Instant
 import java.time.LocalDateTime
 
 @Composable
@@ -43,29 +47,58 @@ fun DatePickerButton(
     dateString: String,
     modifier: Modifier = Modifier
 ) {
-    val context = LocalContext.current
-    val dialogTheme =
-        if (isSystemInDarkTheme()) R.style.DialogThemeDark else R.style.DialogThemeLight
-    val datePickerDialog = DatePickerDialog(
-        context,
-        dialogTheme,
-        { _: DatePicker, newYear: Int, newMonth: Int, newDay: Int ->
-            onChange(
-                LocalDateTime.now()
-                    .withYear(newYear)
-                    .withMonth(newMonth + 1)
-                    .withDayOfMonth(newDay)
-                    .withHour(localDateTime.hour)
-                    .withMinute(localDateTime.minute)
-            )
-        }, localDateTime.year, localDateTime.monthValue - 1, localDateTime.dayOfMonth
-    )
-    OutlinedButton(onClick = datePickerDialog::show, modifier = modifier.clearAndSetSemantics {  }) {
+    var isModalShown by remember { mutableStateOf(false) }
+
+    OutlinedButton(onClick = { isModalShown = true }) {
         Icon(
             Icons.Outlined.Event,
             contentDescription = "Open calendar"
         )
         Spacer(Modifier.size(ButtonDefaults.IconSpacing))
         Text(dateString)
+    }
+    if (isModalShown) {
+        DatePickerModal(
+            onDateSelected = {
+                isModalShown = false
+            },
+            onDismiss = {
+                isModalShown = false
+            }
+        )
+    }
+
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerModal(
+    onDateSelected: (Instant) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val datePickerState = rememberDatePickerState()
+
+    DatePickerDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = {
+                val selectedDateMillis = datePickerState.selectedDateMillis
+                if (selectedDateMillis!=null) {
+                    val selectedInstant = Instant.ofEpochMilli(selectedDateMillis)
+                    onDateSelected(selectedInstant)
+                }
+                onDismiss()
+            }) {
+                Text("OK")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        }
+    ) {
+        DatePicker(state = datePickerState)
     }
 }
